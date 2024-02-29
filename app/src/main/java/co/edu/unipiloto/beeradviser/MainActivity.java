f8b34f40cc1168e7ea62af83c52b78d6a22a40d9
jacobo <juanjacobojaramillo@gmail.com>
2024-02-29 01:33:37 -0500

    Initial commit

---
+++ app/src/main/java/co/edu/unipiloto/beeradviser/MainActivity.java
@@ -0,0 +1,42 @@
+package co.edu.unipiloto.beeradviser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClickFindProd(View view){
+        TextView list = (TextView) findViewById(R.id.brands);
+        Spinner color = (Spinner) findViewById(R.id.Color);
+        String prodType = String.valueOf(color.getSelectedItemId());
+
+        if(prodType.equals("0")){
+            String temp = "1. Productos quimicos" +
+                    "\n2. Productos de construccion";
+
+            list.setText(temp);
+        }
+        if(prodType.equals("1")){
+            String temp = "1. Cereales" +
+                    "\n2. Frutales" +
+                    "\n3. leguminosas";
+            list.setText(temp);
+        }
+        if(prodType.equals("2")){
+            String temp = "1. Agua" +
+                    "\n2. Gasolina" +
+                    "\n3. Petroleo";
+            list.setText(temp);
+        }
+
+    }
+}
